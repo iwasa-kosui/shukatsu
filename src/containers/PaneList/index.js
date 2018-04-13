933bc9edf589e9c59c9f4bdac9c8d1cf0797c0e1
Kosui Iwasa <iwasa@katfuji.lab.tuat.ac.jp>
2018-04-13 11:54:40 +0900

    :+1: Add Share Button

--- src/containers/PaneList/index.js
+++ src/containers/PaneList/index.js
@@ -4,10 +4,16 @@ import { showPane } from '../../actions'
 import './index.css'
 import Pane from '../../containers/Pane'
 import imageSources from '../../images/'
+import {
+  FacebookShareButton, FacebookIcon, FacebookShareCount,
+  GooglePlusShareButton, GooglePlusIcon, GooglePlusShareCount,
+  TwitterShareButton, TwitterIcon,
+} from 'react-share'
 
 const mapStateToProps = state => {
   return {
-    panes: state.paneList.panes
+    panes: state.paneList.panes,
+    clicked: state.paneList.tried
   }
 }
 
@@ -15,16 +21,29 @@ const mapDispatchToProps = dispatch => ({
   showPane: id => dispatch(showPane(id))
 })
 
-const PaneList = ({ panes, showPane }) => {
+const PaneList = ({ panes, clicked, showPane }) => {
+  console.log(panes.filter(pane => pane.visible).length)
   return (
-    <div className="PaneList">
-      {panes.map(pane => {
-        return <Pane
-          key={pane.id}
-          {...pane}
-          onClick={() => showPane(pane.id)}
-        />
-      })}
+    <div>
+      <div class="info">
+        <TwitterShareButton url={window.location.href} title={
+          panes.filter(pane => pane.visible).length == panes.length ?
+          "就活生神経衰弱をプレイしよう！私は" + clicked + "回クリックしてクリアしました #就活神経衰弱" :
+          "就活生神経衰弱をプレイしよう！ #就活神経衰弱"
+        }>
+          <TwitterIcon size={32} round={true} />
+        </TwitterShareButton>
+        <span> {clicked} 回クリックしました</span>
+      </div>
+      <div className="PaneList">
+        {panes.map(pane => {
+          return <Pane
+            key={pane.id}
+            {...pane}
+            onClick={() => showPane(pane.id)}
+          />
+        })}
+      </div>
     </div>
   )
 }
